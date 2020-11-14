0246fdee3037956edda30413c44bb5f35818c40a
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-11-14 15:14:47 +0100

    fix

--- partners/partners_data_spliter.py
+++ partners/partners_data_spliter.py
@@ -2,16 +2,17 @@ import pandas
 import csd_dataset.dataset_header
 import os
 import helpers.console_logger
+import config
 
 
 def split_data(dataset, partners, column_names):
     logger = helpers.console_logger.console_logger("Splitting dataset")
     counter = 1
-    if not os.path.exists('data'):
-        os.makedirs('data')
+    if not os.path.exists(os.path.join(config.config['root_dir'], 'data')):
+        os.makedirs(os.path.join(config.config['root_dir'], 'data'))
     for partner in partners:
         logger.run_execution_timer()
-        dataset.loc[dataset['partner_id'] == partner].to_csv(os.path.join('data', partner), sep='\t', columns=column_names)
+        dataset.loc[dataset['partner_id'] == partner].to_csv(os.path.join(config.config['root_dir'], 'data', partner), sep='\t', columns=column_names)
         logger.log_with_execution_time(f'{counter} / {len(partners)}')
         counter+=1
     logger.log_total_time()
@@ -28,7 +29,7 @@ def save_partners_list(partners):
 
 if __name__ == "__main__":
     logger = helpers.console_logger.console_logger("Reading data")
-    csd = pandas.read_csv('../csd_dataset/CriteoSearchData', sep='\t', names=csd_dataset.dataset_header.columns, low_memory=False)
+    csd = pandas.read_csv(os.path.join(config.config['root_dir'], 'csd_dataset', 'CriteoSearchData'), sep='\t', names=csd_dataset.dataset_header.columns, low_memory=False)
     logger.log_total_time()
 
     logger = helpers.console_logger.console_logger("Extracting list of unique partners")
